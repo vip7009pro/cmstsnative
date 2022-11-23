650b2e43990f965eaf0a1be2f2133f42b5ed5cf3
vip7009pro <hung1893@gmail.com>
2022-11-23 09:17:04 +0700

    sua eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
       rules: {
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': 'warn',
+        'prettier/prettier': ['error', {endOfLine: 'auto'}],
       },
     },
   ],
